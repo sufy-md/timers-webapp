41fe9ef4c72f5479ea6076065efc907465f5e9d0
sufyaniitk <totallynotsufyan@gmail.com>
2025-03-01 22:07:42 +0530

    auth facade, centiseconds, fixed slow clock, and better formatting

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from bottle import Bottle, static_file, template
+from bottle import Bottle, request, response, static_file, template
 
 app = Bottle()
 
@@ -10,8 +10,28 @@ def hello():
 def serve_static(file):
     return static_file(file, root='./static')
 
-@app.route('/<user>/stopwatch')
-def stopwatch(user):
-    return template('stopwatch.html', name=user)
+@app.route('/signup', method=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        username = request.forms.get('username')
+        password = request.forms.get('password')
+
+        pass
+
+    return template("signup.html", error=None)
+
+@app.route('/login', method=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.forms.get('username')
+        password = request.forms.get('password')
+
+        pass
+
+    return template("login.html", error=None)
+
+@app.route('/stopwatch')
+def stopwatch():
+    return template('stopwatch.html')
 
 app.run(host='localhost', port=8080, debug=True)
